c55a398436dccb93cbcfa36deb8786203c8f8e12
zaman_hridoy <zaman.hridoy@simplitaught.com>
2025-05-27 15:59:25 +0600

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
   description: `
     SimpliTaught Token (TAWT) is a cryptocurrency designed specifically for the SimpliTaught ecosystem, which aims to revolutionize the education and learning industry. Built on the Solana blockchain, TAWT serves as a utility token that facilitates payments for educational resources such as books and subscriptions, while also rewarding students and educators for their engagement within the platform. The mission of SimpliTaught Token (TAWT) is to create an accessible, decentralized learning economy where participants can benefit from a transparent and efficient transaction system, ultimately enhancing educational opportunities globally.
   `,
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
